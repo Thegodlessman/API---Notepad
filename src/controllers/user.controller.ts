a0d90177fb502b81893ff9819ead87283a10c9d3
Thegodlessman <luischang16@hotmail.com>
2024-10-17 18:17:02 -0400

    Fix: Se ha solucionado el error de la sobrecarga

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express"
 import User, {IUser} from '../models/user'
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response): Promise<Response | any  > => {
     if (!req.body.email || !req.body.password){
-        res.status(400).json({
+        return res.status(400).json({
             msg: 'Verifique que haya ingresado su correo y su contraseña'
         })
     }
 
     const user = await User.findOne({email: req.body.email})
     if(user){
-        res.status(400).json({
+        return res.status(400).json({
             msg: "El usuario ya existe"
         })
     }
+
+    const newUser = new User(req.body)
+    await newUser.save()
+
+    return res.status(201).json(newUser)
 }
 
 export const login = (req: Request, res: Response) => {
