6be15e09be48770f9bc65efa0c298e4f24c99d7c
Thegodlessman <luischang16@hotmail.com>
2024-10-17 19:56:09 -0400

    Feat: Se ha agregado las rutas especiales y la ruta para iniciar sesion

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors'
+import passport from 'passport';
+import passportMiddleware from './middlewares/passport';
 
 import authRoutes from './routes/auth.routes'
+import specialRoutes from './routes/special.routes'
 
 const app = express();
 
@@ -12,11 +15,14 @@ app.use(morgan('dev'));
 app.use(cors())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize())
+passport.use(passportMiddleware)
 
 app.get('/', (req, res) => {
     res.send(`La API está en:${app.get('port')}`)
 }); 
 
 app.use(authRoutes);
+app.use(specialRoutes);
 
 export default app;
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from "express"
 import User, {IUser} from '../models/user'
+import jwt from 'jsonwebtoken'
+
+import config from "../config/config"
+
+function createToken(user: IUser){
+    return jwt.sign({
+        id: user.id,
+        email: user.email
+    }, config.JWTSecret)
+}
 
 export const register = async (req: Request, res: Response): Promise<Response | any  > => {
     if (!req.body.email || !req.body.password){
@@ -21,6 +31,32 @@ export const register = async (req: Request, res: Response): Promise<Response | 
     return res.status(201).json(newUser)
 }
 
-export const login = (req: Request, res: Response) => {
-    res.send('login')
+export const login = async(req: Request, res: Response): Promise< Response | any > => {
+    if (!req.body.email || !req.body.password){
+        return res.status(400).json({
+            msg: 'Verifique que haya ingresado su correo y su contraseña'
+        })
+    }
+
+    const user = await User.findOne({
+        email: req.body.email
+    })
+
+    if(!user){
+        return res.status(400).json({
+            msg: 'Este usuario no ha sido encontrado'
+        });
+    }
+
+    const isMatch = await user.comparePassword(req.body.password)
+
+    if(isMatch){
+        return res.status(200).json({
+            token: createToken(user)
+        })
+    }
+
+    return res.status(400).json({
+        msg: "El correo o la contraseña son incorrectos"
+    })
 }
