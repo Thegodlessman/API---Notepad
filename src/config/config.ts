bbdff3dcbc367b5064db280be9b3c67648dcd3c2
Thegodlessman <luischang16@hotmail.com>
2024-10-19 00:01:25 -0400

    Feat: la base de datos de mongodb ha sido deploy

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export default{
     JWTSecret: process.env.JWT_SECRET || 'secreto',
     DB: {
-        URI: process.env.MONGODB_URI || 'mongodb+srv://thegodlessman:8TXqQS0NeBgtgxq9@cluster.z8qs0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster', 
+        URI: process.env.MONGODB_URI || 'mongodb://localhost/jwttutorial', 
         USER: process.env.MONGODB_USER  || '', 
         PASSWORD: process.env.MONGODB_PASSWORD || ''
     }
