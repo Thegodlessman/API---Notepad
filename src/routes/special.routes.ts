5f41d23ccc424d32083636cde681fff1f0ac7f35
Thegodlessman <luischang16@hotmail.com>
2024-10-25 20:10:49 -0400

    Feat: se ha cambiado el metodo de showFav de get a post

--- src/routes/special.routes.ts
+++ src/routes/special.routes.ts
@@ -17,7 +17,7 @@ const router = Router();
 router.post('/special', passport.authenticate('jwt', {session: false}), createNote);
 router.post('/createCat', passport.authenticate('jwt', {session: false}), createCategory);
 router.patch('/setFav/:id',  passport.authenticate('jwt', {session: false}), setFavorite );
-router.get('/showFav', passport.authenticate('jwt', {session: false}), showFav);
+router.post('/showFav', passport.authenticate('jwt', {session: false}), showFav);
 router.post('/showNotes', passport.authenticate('jwt', {session: false}), showNotes);
 router.get('/showCat',  passport.authenticate('jwt', {session: false}), showCat);
 router.delete('/deleteNote/:id', passport.authenticate('jwt', {session: false}), deleteNote);
