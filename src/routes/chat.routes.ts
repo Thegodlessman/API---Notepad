7902c8a43ea15858c091b468d1fff071187837df
Thegodlessman <luischang16@hotmail.com>
2024-12-12 13:30:19 -0400

    Fix 2

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -3,7 +3,7 @@ import { getFriends, getMessages } from "../controllers/chat.controller";
 
 const router = express.Router();
 
-router.get("/friends", getFriends);
+router.post("/friends", getFriends);
 
 router.get("/:userId/:friendId", getMessages);
 
